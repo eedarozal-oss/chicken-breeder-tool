d29a0257ba13132b8042d9cb0088934d4d84638c
eedarozal-oss <ardeelazo@gmail.com>
2026-03-30 22:41:29 +0800

    Add files via upload
    added loading of cache data

--- chicken-breeder-tool-deploy/services/database.py
+++ chicken-breeder-tool-deploy/services/database.py
@@ -3,6 +3,7 @@ from services.db.chickens import (
     upsert_chicken,
     get_chicken_by_token,
     get_chickens_by_wallet,
+    get_static_chickens_by_token_ids,
 )
 from services.db.family_roots import (
     clear_family_roots_for_wallet,
@@ -12,6 +13,7 @@ from services.db.family_roots import (
     insert_family_root_items,
     get_family_root_items,
     upsert_family_root_item,
+    preload_cached_family_roots_for_wallet,
 )
 
 __all__ = [
@@ -19,11 +21,13 @@ __all__ = [
     "upsert_chicken",
     "get_chicken_by_token",
     "get_chickens_by_wallet",
+    "get_static_chickens_by_token_ids",
     "clear_family_roots_for_wallet",
     "clear_family_roots_for_token",
     "clear_stale_family_root_summaries",
     "get_family_root_items",
     "upsert_family_root_item",
+    "preload_cached_family_roots_for_wallet",
     "upsert_family_root_summary",
     "insert_family_root_items",
 ]
